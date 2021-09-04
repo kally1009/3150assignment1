5dd208ca56ebe5225378f05a510788ab4c9f4887
Kalicia Adams <kally1009@kalicia-laptop.localdomain>
2021-09-04 15:20:27 -0600

    Changes made to testing

--- hash_test.py
+++ hash_test.py
@@ -6,7 +6,7 @@ import hash
 
 
 class TestStudent(unittest.TestCase):
-    def setup(self):
+    def setUp(self):
         pass
 
     def test_getSSN(self):
@@ -29,31 +29,54 @@ class TestStudent(unittest.TestCase):
         #How do I test these kinds of functions with overriding what == , >, <, and int?
     
     def test_isPrime_Prime(self): # not within a class...? 
-        self.assertTrue(hash.isPrime(3),True) #Testing if 3 is prime? What do I do with self.something? Function not in a class. 
-        #test if 1 and 2 and 5 and 7 and 9 is prime 
+        self.assertTrue(hash.isPrime(3),True) 
+        self.assertTrue(hash.isPrime(1),True)
+        self.assertTrue(hash.isPrime(2), True)
+        self.assertTrue(hash.isPrime(5),True)
+        self.assertTrue(hash.isPrime(7), True)
+        self.assertTrue(hash.isPrime(19), True) 
         #test high numbers if prime or not
         #test negative numbers
-        
+    
+    def test_isPrime_Prime_largeNumbers(self):
+        self.assertTrue(hash.isPrime(487), True)
+        self.assertTrue(hash.isPrime(499), True)
+        self.assertTrue(hash.isPrime(6971), True)
+        self.assertTrue(hash.isPrime(9679), True)
+        self.assertTrue(hash.isPrime(11587), True)
+        self.assertTrue(hash.isPrime(33469), True)
+
+
     def test_isPrime_notPrime(self):
-        #self.assertFalse(self(4),False)
+        self.assertFalse(hash.isPrime(4),False)
+        self.assertFalse(hash.isPrime(6),False)
+        self.assertFalse(hash.isPrime(8),False)
+        self.assertFalse(hash.isPrime(100), False)
+
+    def test_isPrime_notPrime_LargeNumbers(self):
+        self.assertFalse(hash.isPrime(100000), False)
+        self.assertFalse(hash.isPrime(12000), False)
+        self.assertFalse(hash.isPrime(2563), False)
+
+    ##  self.assertFalse(hash.isPrime(-1),False) -> Raises an error, how to have it be expecting an error?
+        #self.assertFalse(hash.isPrime(-7), False)
         #test if 4, 6, 8, 0? 
         #test high numbers
         #test negative numbers
-        pass
+        
 
-    def test_isPrime_notNone(self):
-        pass
+   # def test_isPrime_notNone(self):
+        #pass
         #self.assertIsNotNone 
         #test if x isn't or is none, and handled correctly (instead of crashing?)
 
-    def test_isPrime_None(self):
-        pass
+    #def test_isPrime_None(self):
+        #pass
         #test if it is none
 
-    def test_hash_init(self):
-        pass
-        #test the __init__ (figure out what exactly it is doing) Look at Bart's instructions on assignment?
-        #
+    def test_hash_init_msize(self):
+        newHash = hash.Hash(10)
+        self.assertEqual(newHash.size(),0)
     def test_Insert(self):  #how to test this with files? Need to include files within test suite?
         pass
     
